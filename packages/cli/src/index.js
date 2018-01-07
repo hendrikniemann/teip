dcd2ca521f32ea8209b843c5c3c3878f89bdad61
hendrikniemann <hendrik.niemann@kiron.ngo>
2018-01-07 14:10:13 +0100

    Create implementation of graphqlconfig powered cli
    This simple implementation utilises the graphqlconfig file to locate
    schema definitions for .grahql and .gql files and produces query type
    definitions for these files next to them by appending .flow to the
    filename.

---
+++ packages/cli/src/index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+/* @flow */
+import { parse, buildSchema, GraphQLSchema } from 'graphql';
+import { documentToModule } from '@teip/create-flow-types';
+import babelGenerator from '@babel/generator';
+import commander from 'commander';
+import fs from 'fs';
+import path from 'path';
+import glob from 'glob';
+import { getGraphQLConfig } from 'graphql-config';
+import pkg from '../package.json';
+
+const DEFAULT_MATCHER = '**/*.g*(raph)ql';
+
+const schemaCache: Map<string, GraphQLSchema> = new Map();
+
+commander
+  .version(pkg.version)
+  .command('generate [pattern]')
+  .description('Generate flow types for files')
+  .action(async (pattern = DEFAULT_MATCHER) => {
+    const files = glob.sync(pattern);
+    const globalConfig = getGraphQLConfig(process.cwd());
+    const writtenFiles = files.forEach(file => {
+      const config = globalConfig.getConfigForFile(file);
+      if (config.schemaPath !== path.resolve(file)) {
+        let schema: ?GraphQLSchema = schemaCache.get(config.schemaPath);
+        if (!schema) {
+          schema = buildSchema(fs.readFileSync(config.schemaPath).toString('utf8'));
+          schemaCache.set(config.schemaPath, schema);
+        }
+        const fileContent = fs.readFileSync(path.resolve(process.cwd(), file));
+
+        const ast = parse(fileContent.toString('utf8'));
+        const generated = babelGenerator(documentToModule(ast, schema)).code;
+        fs.writeFileSync(path.resolve(process.cwd(), `${file}.flow`), generated);
+      }
+    });
+  });
+
+commander.parse(process.argv);
